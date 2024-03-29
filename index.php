542fedecc9b29c5c1d2e6501eaea68215d96afdb
smile1outlook <153255193+smile1outlook@users.noreply.github.com>
2024-03-29 17:17:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -121,5 +121,18 @@ foreach ($availabilityDomains as $availabilityDomainEntity) {
         $notifier->notify($message);
     }
 
+    $url = "https://api.example.com/data";
+    $data = $message"
+
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+
+    $response = curl_exec($curl);
+    curl_close($curl);
+
+    echo $response;
+
     return;
 }
